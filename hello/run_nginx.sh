f2c7284e9e86741076c8177e52982ec8a4f78ca5
Tangzharyk <90594377+0tang1@users.noreply.github.com>
2022-02-23 09:06:39 +0000

    hello docker (6)

--- hello/run_nginx.sh
+++ hello/run_nginx.sh
@@ -2,4 +2,4 @@
 set -eu
 
 # you code here ....
-docker run -d --name nginx-container --network=host -p localhost:80 nginx:1.19
+docker run --name nginx-container --network=host -p localhost:80 --detach nginx:1.19
