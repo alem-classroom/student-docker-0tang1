f44216a11a9e9a33a448b7732a98322d138d2416
Atavinci <vunderkind980123@gmail.com>
2022-02-25 23:08:46 +0600

    hello docker (11)

--- hello/run_nginx.sh
+++ hello/run_nginx.sh
@@ -4,4 +4,4 @@ set -eu
 # you code here ....
 # docker stop nginx-container
 # docker rm nginx-container
-docker run -d --name nginx-container -p 80:80 nginx:1.19
+docker run -d --name nginx-container --network=host -p 80:80 nginx:1.19
