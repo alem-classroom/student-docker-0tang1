7932522dd568e3320994195b9c2b9ffed086c7dd
Tangzharyk <90594377+0tang1@users.noreply.github.com>
2022-02-25 17:41:04 +0000

    hello docker

--- hello/run_nginx.sh
+++ hello/run_nginx.sh
@@ -4,4 +4,4 @@ set -eu
 # you code here ....
 # docker stop nginx-container
 # docker rm nginx-container
-docker run -i -t -d --name nginx-container --network=host tail -f -p localhost:80 nginx:1.19
+docker run -d --name nginx-container -P 80:80 nginx:1.19
